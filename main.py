355df6983a7d05fb8b468db177a4ec2b038fbfac
neqq <supitcha.psr@gmail.com>
2024-03-16 02:20:14 +0700

    refactor: adjust age range mapping function to create 13 ranges

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ st.header('Input Features')
 # Define mappings for categorical features
 diabetes_map = {0: 'No Diabetes', 1: 'Pre-diabetes', 2: 'Diabetes'}
 gen_hlth_map = {1: 'Excellent', 2: 'Good', 3: 'Average', 4: 'Fair', 5: 'Poor'}
-pred_map = {0: 'No risk of Heart Disease found', 1: 'Having s high risk of Heart Disease'}
+pred_map = {0: 'No risk of Heart Disease found', 1: 'Having high risk of Heart Disease'}
 
 # Input features
 high_bp = st.radio('High Blood Pressure', ['No', 'Yes'])
@@ -32,8 +32,36 @@ gen_hlth = st.select_slider('General Health', options=list(gen_hlth_map.values()
 phys_hlth = st.slider('Physical Health', min_value=0, max_value=30, step=1)
 diff_walk = st.radio('Difficulty Walking', ['No', 'Yes'])
 sex = st.radio('Sex', ['Man', 'Woman'])
-age = st.number_input('Age', min_value=1, max_value=13, step=1)
+age = st.number_input('Age', min_value=18, max_value=100, step=1)
 
+def get_age_mapping(age):
+    if age >= 18 and age <= 24:
+        return 1
+    elif age >= 25 and age <= 29:
+        return 2
+    elif age >= 30 and age <= 34:
+        return 3
+    elif age >= 35 and age <= 39:
+        return 4
+    elif age >= 40 and age <= 44:
+        return 5
+    elif age >= 45 and age <= 49:
+        return 6
+    elif age >= 50 and age <= 54:
+        return 7
+    elif age >= 55 and age <= 59:
+        return 8
+    elif age >= 60 and age <= 64:
+        return 9
+    elif age >= 65 and age <= 69:
+        return 10
+    elif age >= 70 and age <= 74:
+        return 11
+    elif age >= 75 and age <= 79:
+        return 12
+    else:
+        return 13
+    
 def predict_input_data(model, input_data, scaler):
     scaled_input_data = scaler.transform(input_data.reshape(1, -1))
     prediction = model.predict(scaled_input_data)
@@ -51,8 +79,9 @@ if predict_btn_clicked:
     gen_hlth_numeric = next(key for key, value in gen_hlth_map.items() if value == gen_hlth)
     sex_numeric = 1 if sex == 'Man' else 0
     diff_walk_numeric = 1 if diff_walk == 'Yes' else 0
+    age_numeric = get_age_mapping(age)
 
-    input_data = np.array([high_bp_numeric, high_chol_numeric, smoker_numeric, stroke_numeric, diabetes_numeric, gen_hlth_numeric, phys_hlth, diff_walk_numeric, sex_numeric, age])
+    input_data = np.array([high_bp_numeric, high_chol_numeric, smoker_numeric, stroke_numeric, diabetes_numeric, gen_hlth_numeric, phys_hlth, diff_walk_numeric, sex_numeric, age_numeric])
     
     prediction = predict_input_data(model, input_data, scaler)
     st.balloons()
